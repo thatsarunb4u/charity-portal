8b122d4ca2bcb4b34988dbb8e6edcc32ac10c777
Arun <thatsarunb4u@gmail.com>
2026-07-24 21:05:41 +0800

    Dynamic loading of homepage

--- app/thank-you/[ticket]/page.tsx
+++ app/thank-you/[ticket]/page.tsx
@@ -2,6 +2,8 @@ import { supabaseAdmin } from "@/lib/supabaseAdmin";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function ThankYouPage({
   params,
 }: {
